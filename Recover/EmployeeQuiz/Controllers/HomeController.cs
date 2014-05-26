e4d5b067da82d4c32c6cfcd9e1e5440bb20426e3
zulma <zulmatec@hotmail.com>
2014-05-25 22:31:44 -0500

    Recopueracion Recover

---
+++ Recover/EmployeeQuiz/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeQuiz.Models;
+
+namespace EmployeeQuiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Employee emp)
+        {
+            //creo el objeto del modelo de datos
+            
+                PayrollDm nomina = new PayrollDm(
+                  @"C:\Users\zulma\Documents\Noveno\DAW\Recover\EmployeeQuiz\Models\BaseEmpleados.csv");
+
+                //Busco el empleado dado su ID
+                var empleado = nomina.GetEmployeeById(emp.Id);
+                if (ModelState.IsValid)
+                {
+
+                    return View("WorkerView", empleado);
+                }
+                else
+                {
+                    return View();
+                }
+           
+        }
+
+        public ActionResult NotFound()
+        {
+            ViewBag.ErrorCode = "NF0001";
+            ViewBag.Description = "La mascota no se encuestra" +
+               "en la base de datos";
+            return View();
+        }
+    }
+}
