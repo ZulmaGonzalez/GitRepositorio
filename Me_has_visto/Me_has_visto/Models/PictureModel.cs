e73173e76c5c2911d6b1952c033e240a7b23c0fb
zulma <zulmatec@hotmail.com>
2014-06-08 21:48:39 -0500

    Me has visto

---
+++ Me_has_visto/Me_has_visto/Models/PictureModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Linq;
+using System.Web;
+
+namespace Me_has_visto.Models
+{
+    public class PictureModel
+    {
+        [Required]
+        public HttpPostedFileBase PictureFile { get; set; }
+    }
+}
